9d51575623b86b04e1236b91eeb52ead65a5b5de
Данила Семешкин <semyoshkin8v@mail.ru>
2022-04-20 22:45:18 +0300

    Base Command

--- WPF_MVVM/ViewModels/MainWindowViewModel.cs
+++ WPF_MVVM/ViewModels/MainWindowViewModel.cs
@@ -7,5 +7,15 @@ namespace WPF_MVVM.ViewModels
 {
     public sealed class MainWindowViewModel : ViewModel
     {
+        #region Properties
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        #endregion
     }
 }
