ac9c98a9acd3749432f002f95bfd94d9b5d69969
Данила Семешкин <semyoshkin8v@mail.ru>
2022-04-21 20:09:43 +0300

    CloseApplicationCommand

--- WPF_MVVM/ViewModels/MainWindowViewModel.cs
+++ WPF_MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using WPF_MVVM.Infrastructure.Commands;
 using WPF_MVVM.ViewModels.Base;
 
 namespace WPF_MVVM.ViewModels
 {
     public sealed class MainWindowViewModel : ViewModel
     {
+        #region Constructors
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+        }
+
+        #endregion
+
         #region Properties
 
         private string _title;
@@ -15,6 +26,23 @@ namespace WPF_MVVM.ViewModels
             get => _title;
             set => Set(ref _title, value);
         }
+
+        #endregion
+
+        #region Commands
+
+        #region Close Application Command
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        #endregion
 
         #endregion
     }
