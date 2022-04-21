5747bc5d8e8b5b8160384a5a59a808ea5ccd0a67
Данила Семешкин <semyoshkin8v@mail.ru>
2022-04-21 21:38:19 +0300

    Извлечение дат

--- Tests/WPF_MVVM_Console/Program.cs
+++ Tests/WPF_MVVM_Console/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace WPF_MVVM_Console
 {
@@ -7,12 +13,41 @@ namespace WPF_MVVM_Console
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-        static void Main(string[] args)
+        private static async Task<Stream> GetDataStream()
         {
             var client = new HttpClient();
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
 
-            var response = client.GetAsync(data_url).Result;
-            string csv_str = response.Content.ReadAsStringAsync().Result;
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
+
+
+        static void Main(string[] args)
+        {
+            
         }
     }
 }
