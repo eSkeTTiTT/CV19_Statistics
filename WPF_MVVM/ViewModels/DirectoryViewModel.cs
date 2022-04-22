5c7eaca294a37c693bcaea0eae28fdebf549a738
Данила Семешкин <semyoshkin8v@mail.ru>
2022-04-22 21:57:27 +0300

    DirectoryViewModel + FileViewModel

---
+++ WPF_MVVM/ViewModels/DirectoryViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WPF_MVVM.ViewModels.Base;
+
+namespace WPF_MVVM.ViewModels
+{
+    public sealed class DirectoryViewModel : ViewModel
+    {
+        private readonly DirectoryInfo _directoryInfo;
+
+        public IEnumerable<DirectoryViewModel> SubDirectories => _directoryInfo
+            .EnumerateDirectories()
+            .Select(dir_info => new DirectoryViewModel(dir_info.FullName));
+
+        public IEnumerable<FileViewModel> Files => _directoryInfo
+            .EnumerateFiles()
+            .Select(file => new FileViewModel(file.FullName));
+
+        public IEnumerable<object> DirectoryItems => SubDirectories.Cast<object>().Concat(Files);
+
+        public string Name => _directoryInfo.Name;
+        public string Path => _directoryInfo.FullName;
+        public DateTime CreationTime => _directoryInfo.CreationTime;
+
+        public DirectoryViewModel(string path)
+        {
+            _directoryInfo = new DirectoryInfo(path);
+        }
+    }
+}
