da7175777416606af8186b8cb98747377941f97e
Данила Семешкин <semyoshkin8v@mail.ru>
2022-05-01 19:56:53 +0300

    Новая точка входа - класс Program

---
+++ WPF_MVVM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_MVVM
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
