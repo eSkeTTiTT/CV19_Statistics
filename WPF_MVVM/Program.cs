49f594451b2b54609205711f89ab3753803023a4
Данила Семешкин <semyoshkin8v@mail.ru>
2022-05-01 20:42:16 +0300

    Запуск хоста при старте приложения

--- WPF_MVVM/Program.cs
+++ WPF_MVVM/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,6 +14,22 @@ namespace WPF_MVVM
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(args);
+
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+            return host_builder;
         }
     }
 }
