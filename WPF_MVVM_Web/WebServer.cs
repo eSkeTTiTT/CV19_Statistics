2f3436385055f6d2ab38eb6c3f83fa9d102e55a5
Данила Семешкин <semyoshkin8v@mail.ru>
2022-05-05 22:37:39 +0300

    Базовая реализация сервера

--- WPF_MVVM_Web/WebServer.cs
+++ WPF_MVVM_Web/WebServer.cs
@@ -8,6 +8,8 @@ namespace WPF_MVVM_Web
 {
     public class WebServer
     {
+        private event EventHandler<RequestReceivedEventArgs> RequestReceived;
+
         //private TcpListener _listener = new TcpListener(new IPEndPoint(IPAddress.Any, 8080));
         private HttpListener _listener;
         private readonly int _port;
@@ -39,8 +41,8 @@ namespace WPF_MVVM_Web
                 _listener.Prefixes.Add($"http://*:{_port}");
                 _listener.Prefixes.Add($"http://+:{_port}");
                 _enabled = true;
+                ListenAsync();
             }
-            Listen();
         }
 
         public void Stop()
@@ -54,9 +56,31 @@ namespace WPF_MVVM_Web
             }
         }
 
-        private void Listen()
+        private async void ListenAsync()
         {
+            var listener = _listener; // чтобы если ссылка изменена - продожить работу
 
+            listener.Start();
+
+            while (_enabled)
+            {
+                var context = await listener.GetContextAsync().ConfigureAwait(false);
+                ProcessRequest(context);
+            }
+
+            listener.Stop();
         }
+
+        private void ProcessRequest(HttpListenerContext context)
+        {
+            RequestReceived?.Invoke(this, new RequestReceivedEventArgs(context));
+        }
+    }
+
+    public class RequestReceivedEventArgs : EventArgs
+    {
+        public HttpListenerContext Context { get; }
+
+        public RequestReceivedEventArgs(HttpListenerContext context) => Context = context;
     }
 }
